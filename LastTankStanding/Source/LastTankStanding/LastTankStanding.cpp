f69c06ed9ba2494b3f572f01f5695663757fab12
Shravan Kumar <kum_shravan@yahoo.com>
2016-09-19 21:08:59 +0530

    Unreal project created.

---
+++ LastTankStanding/Source/LastTankStanding/LastTankStanding.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LastTankStanding.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastTankStanding, "LastTankStanding" );
---
+++ LastTankStanding/Source/LastTankStanding/LastTankStandingGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LastTankStandingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTTANKSTANDING_API ALastTankStandingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
