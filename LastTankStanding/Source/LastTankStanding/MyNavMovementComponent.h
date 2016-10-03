4f30a9ed4f671095a5532e28606b531d1d522380
Shravan Kumar <kum_shravan@yahoo.com>
2016-10-03 22:23:54 +0530

    Added a new tank movement component.

---
+++ LastTankStanding/Source/LastTankStanding/MyNavMovementComponent.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "MyNavMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTTANKSTANDING_API UMyNavMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
